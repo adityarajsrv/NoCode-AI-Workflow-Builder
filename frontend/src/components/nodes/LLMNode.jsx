8cd1bfd2f591c15dc80e1525939a67649f05e043
Aditya <adityarajsrvofficial@gmail.com>
2025-10-23 05:56:01 +0530

    Updated LLMNode

--- frontend/src/components/nodes/LLMNode.jsx
+++ frontend/src/components/nodes/LLMNode.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Settings, Eye, Sparkles, Unlink, Trash2 } from 'lucide-react';
